4f5bfbe36de399703d7cdb6ef9c0f01cd264bceb
Jorman <tille100@gmail.com>
2012-09-29 12:05:51 -0500

    initial commit

--- src/PrincipalCenaSalvajes.java
+++ src/PrincipalCenaSalvajes.java
@@ -4,29 +4,21 @@ import co.edu.eafit.dis.st0257.cenasalvajes.Cocinero;
 
 public class PrincipalCenaSalvajes {
 
-    public static void usage() {
-	System.err.println("Usage: java MainCenaSalvaje " +
-			   " <nSalvajes> <nPorcioens>");
-	System.exit(1);
+  public static void usage() {
+    System.err.println("Usage: java MainCenaSalvaje " + " <nSalvajes> <nPorcioens>");
+    System.exit(1);
+  }
+
+  public static void main(String args[]) {
+    if (args.length != 2 || nSalvajes <= 0 || nPorciones <= 0) usage();
+  
+    int nSalvajes = Integer.parseInt(args[0]);
+    int nPorciones = Integer.parseInt(args[1]);
+    Olla olla = new Olla(nPorciones);
+  
+    new Thread(new Cocinero(olla)).start();
+    for (int i = 0; i < nSalvajes; ++i) {
+      new Thread(new Salvaje(i, olla)).start();
     }
-
-    public static void main(String args[]) {
-	if (args.length != 2) {
-	    usage();
-	}
-	int nSalvajes = Integer.parseInt(args[0]);
-	int nPorciones = Integer.parseInt(args[1]);
-
-	if (nSalvajes <= 0 || nPorciones <= 0) {
-	    usage();
-	}
-
-	Olla olla = new Olla(nPorciones);
-
-	new Thread(new Cocinero(olla)).start();
-
-	for (int i = 0; i < nSalvajes; i++) {
-	    new Thread(new Salvaje(i, olla)).start();
-	}
-    }
+  }
 }
--- src/co/edu/eafit/dis/st0257/cenasalvajes/Cocinero.java
+++ src/co/edu/eafit/dis/st0257/cenasalvajes/Cocinero.java
@@ -1,35 +1,29 @@
 package co.edu.eafit.dis.st0257.cenasalvajes;
 
-public class Cocinero
-    implements Runnable {
+public class Cocinero implements Runnable {
+  private Olla olla;
+  
+  public Cocinero(Olla olla) {
+    this.olla = olla;
+  }
 
-    private Olla olla;
-
-    public Cocinero(Olla olla) {
-
-	this.olla = olla;
+  public void run() {
+    while (true) {
+      System.out.println(this.toString() + " a domir");
+      dormir();
+      System.out.println(this.toString() + " a cocinar");
+      olla.ponerPorciones();
     }
+  }
 
-    public void run() {
-
-	while (true) {
-	    System.out.println(this.toString() +
-			       " a domir");
-	    dormir();
-	    System.out.println(this.toString() +
-			       " a cocinar");
-	    olla.ponerPorciones();
-	}
+  public void dormir() {
+    try {
+      Thread.sleep(10000);
+    } catch (InterruptedException ie) {
     }
+  }
 
-    public void dormir() {
-	try {
-	    Thread.sleep(10000);
-	} catch (InterruptedException ie) {
-	}
-    }
-
-    public String toString() {
-	return "Cocinero ";
-    }
+  public String toString() {
+    return "Cocinero ";
+  }
 }
